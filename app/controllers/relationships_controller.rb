90884e4937ec526f8cee5d846f7b28eaf6135db3
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-06-05 15:03:29 +0000

    Update

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,14 @@
 class RelationshipsController < ApplicationController
-   before_action :set_user
+   before_action :set_user, only: [:create, :destroy]
 
   def create
     following = current_user.follow(@user)
     if following.save
       flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
+      redirect_back(fallback_location: root_path)
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
+      redirect_back(fallback_location: root_path)
     end
   end
 
@@ -16,18 +16,29 @@ class RelationshipsController < ApplicationController
     following = current_user.unfollow(@user)
     if following.destroy
       flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
+      redirect_back(fallback_location: root_path)
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
+      redirect_back(fallback_location: root_path)
     end
+  end
+
+
+  def follows
+    @user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = user.followers
+  end
 
 
   private
-  def
-    @user = User.find(params[:relationship][:follow_id])
+  def set_user
+    @user = User.find(params[:follow_id])
   end
 
-  
 
-  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,20 @@ Rails.application.routes.draw do
 
   devise_for :users
 # deviseは先
+
   resources :users,only: [:show,:index,:edit,:update]
+  resources :relationships, only: [:create, :destroy]
+  get '/relationships/follows' => 'relationships#follows'
+  get '/relationships/followers' => 'relationships#followers'
+
+
 
   resources :books, only: [:new, :create, :edit, :update, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
 
-  resources :relationships, only: [:create, :destroy]
+
 
   root 'homes#top'
   get '/home/about' => 'homes#about'
